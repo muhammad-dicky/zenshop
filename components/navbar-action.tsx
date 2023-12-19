5d781a0277ae8ecb2a6b2f8ffde802d5e7727239
Dicky <muhammad.dickynn@gmail.com>
2023-12-20 00:09:51 +0700

    Add .env file and fetch categories in navbar component

---
+++ components/navbar-action.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const NavbarActions = () => {
+    return ( 
+        <div className="ml-auto flex items-center gap-x-4">
+            Hello actions
+        </div>
+     );
+}
+ 
+export default NavbarActions;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Container from '@/components/ui/container'
 import Link from 'next/link'
 import MainNav from './main-nav'
+import getCategories from '@/actions/get-categories';
+import NavbarAction from './navbar-action';
 
-const navbar = () => {
+export const revalidate = 0;
+
+const navbar = async () => {
+  const categories = await getCategories();
+
   return (
     <div className='border-b'>
         <Container>
@@ -11,11 +17,12 @@ const navbar = () => {
               <Link href={'/'} className='ml-4 flex lg:ml-0 gap-x-2'>
               <p className='font-bold text-xl'>STORE</p>
               </Link>
-              <MainNav data={[]}/>
+              <MainNav data={categories} />
+              <NavbarAction/>
             </div>
         </Container>
     </div>
   )
 }
 
-export default navbar
+export default navbar 
