8cffbd33bde49a0859b2baac687e06853cada0fe
Dicky <muhammad.dickynn@gmail.com>
2023-12-22 01:34:34 +0700

    Add Gallery component and update dependencies

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 import ProductCard from "@/components/ui/product-card";
@@ -9,12 +10,11 @@ interface ProductPageProps {
     params: {
         productId: string;
     }
-    items: Product[];
+    
 }
 
 const ProductPage: React.FC<ProductPageProps> = async ({
-    params,
-    items
+    params
 }) => {
     const product = await getProduct(params.productId);
     const suggestedProducts = await getProducts({
@@ -26,7 +26,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                         {/* GALLERY */}
-                        <div>Gallery</div>
+                        <Gallery images={product.images}/>
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
                             {/* Info */}
                             Info
