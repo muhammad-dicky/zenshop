4df5a87c4017c8d64db4643827f9dfa21e07673b
Dicky <muhammad.dickynn@gmail.com>
2023-12-23 17:08:25 +0700

    Add mobile filters and display no result message if products are empty

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,9 @@ import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 import Filter from "./components/filter";
+import NoResult from "@/components/ui/no-result";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -41,10 +44,22 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             <Billboard data={category.billboard}/>
             <div className="px-4 sm:px-6 lg:px-8 pb-24">
                 <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-                    {/*TODO : Add Mobile Filters */}
+
+                    <MobileFilters sizes={sizes} colors={colors}/>
+
                     <div className="hidden lg:block">
                         <Filter data={sizes} name="Sizes" valueKey="sizeId" />
+                        <Filter data={colors} name="Colors" valueKey="colorId" />
                         
+                    </div>
+                    <div className="mt-6 lg:col-span-4 lg:mt-0">
+                        {products.length === 0 && <NoResult/>}
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                            {products.map((item) => (
+                                <ProductCard key={item.id} data={item}/>
+                            ))}
+
+                        </div>
                     </div>
                 </div>
             </div>
