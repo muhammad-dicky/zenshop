89c8636b28ded9765572505c8d7a4540f63737f2
Dicky <muhammad.dickynn@gmail.com>
2023-12-26 23:20:46 +0700

    Commented out unused code in CartPage component

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -8,16 +8,17 @@ import { useEffect, useState } from "react";
 
 
 const CartPage = () => {
-    const [isMounted, setIsMounted] = useState(false);
+    // const [isMounted, setIsMounted] = useState(false);
 
     const cart = useCart();
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-    if(!isMounted){
-        return null;
-    }
+    // useEffect(() => {
+    //     setIsMounted(true);
+    // }, []);
+    
+    // if(!isMounted){
+    //     return null;
+    // }
 
     return ( 
         <div className="bg-white">
