9cf12922d6b6f9195edc61feb80029115c399155
Dicky <muhammad.dickynn@gmail.com>
2024-02-18 14:51:33 +0700

    Refactor components and fix formatting

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { cn } from "@/lib/utils";
 import { Category } from "@/types";
@@ -6,32 +6,34 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 interface MainNavProps {
-    data:Category[]
+  data: Category[];
 }
 
-const MainNav:React.FC<MainNavProps> = ({
-    data
-}) => {
-    const pathname = usePathname();
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
+  const pathname = usePathname();
 
-    const routes = data.map((route) => ({
-        href: `/category/${route.id}`,
-        label: route.name,
-        active: pathname === `/category/${route.id}`
-    }))
+  const routes = data?.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
 
-
-    return ( 
-       <nav className="mx-6 items-center space-x-4 lg:space-x-6">
-        {routes.map((route) => (
-            <Link key={route.href} href={route.href} className={cn("text-sm font-medium transition-colors hover:text-black",
+  return (
+    <nav className="mx-6 items-center space-x-4 lg:space-x-6">
+      {routes?.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-black",
             route.active ? "text-black" : "text-neutral-500"
-            )}>
-            {route.label}
-            </Link>
-        ))}
-       </nav>
-     );
-}
- 
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
 export default MainNav;
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -4,27 +4,22 @@ import Container from "./ui/container";
 import ProductCard from "./ui/product-card";
 
 interface ProductProps {
-    title: string;
-    items: Product[];
+  title: string;
+  items: Product[];
 }
 
+const ProductList: React.FC<ProductProps> = ({ title, items }) => {
+  return (
+    <div className="m-5 space-y-4">
+      <h3 className="font-bold text-3xl">{title}</h3>
+      {items?.length === 0 && <NoResult />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {items?.map((item) => (
+          <ProductCard key={item.id} data={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const ProductList: React.FC<ProductProps> = ({
-    title,items
-}) => {
-    return ( 
-        <div className="m-5 space-y-4">
-            <h3 className="font-bold text-3xl">
-            {title}
-            </h3>
-            {items.length === 0 && <NoResult/>}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                {items.map((item) => (
-                        <ProductCard key={item.id} data={item}/>
-                ))}
-            </div>
-        </div>
-     );
-}
- 
 export default ProductList;
